7173cab6be8518d70b10146da790eb24dbffdc85
manuel <manuel@gide.net>
2021-10-08 13:11:31 +0200

    (refacto) lint + architecture de base

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="@/assets/logo.png" alt="" width="50" height="50" class="d-inline-block align-text-top">
+                Test Free2move
+            </a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        msg: String
+    }
+}
+</script>
---
+++ src/views/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Header />
+        <div class="container">
+            <slot />
+        </div>
+        <footer>Test Free2move - Manuel Landreau</footer>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+    components: {
+        Header
+    }
+}
+</script>
